42365da3833b9e36ba2e0d478f9be4528d63dd8e
Milan Raj <rajsite@users.noreply.github.com>
2017-01-11 23:47:38 -0600

    Add verbose logging to install script

--- bin/i3-docs.js
+++ bin/i3-docs.js
@@ -8,17 +8,103 @@
     var path = require('path');
     var shelljs = require('shelljs');
     var findParentDir = require('find-parent-dir');
+    var pathIsInside = require('path-is-inside');
     var yargs = require('yargs');
 
-    var loadI3DocsConfig = function (configPath) {
+    var log = function () {
+        // Intentionally blank
+    };
+
+    var createLogger = function (verbose) {
+        if (verbose) {
+            log = console.log.bind(console);
+        }
+    };
+
+    var findI3DocsConfigDir = function (cwd) {
+        var configDir = findParentDir.sync(cwd, '.i3-docs.ini');
+
+        if (configDir === null) {
+            console.error('Could not find .i3-docs.ini config file in current working directory or parent directory.');
+            console.error(' - Current working directory:', cwd);
+            process.exit(1);
+        }
+
+        return configDir;
+    };
+
+    var loadI3DocsConfig = function (configDir) {
+        var configPath = path.join(configDir, '.i3-docs.ini');
+        log('Configuration file (.i3-docs.ini) path:', configPath);
+
         var configText = fs.readFileSync(configPath, 'utf-8');
         var config = ini.parse(configText);
 
+        if (config.output_directories === undefined) {
+            console.error('The .i3-docs.ini config file needs an output_directories section');
+            process.exit(1);
+        }
+
         if (Object.keys(config.output_directories).length < 1) {
-            console.error('The .i3-docs.ini config file needs an output directory');
+            console.error('The .i3-docs.ini output_directories section needs at least one project specified');
+            process.exit(1);
         }
 
         return config;
+    };
+
+    var createAbsoluteOutputDirectoryEntries = function (configDir, config) {
+        log('Identifying projects in .i3-docs.ini [start]');
+        var absoluteOutputDirectoryEntries = {};
+        Object.keys(config.output_directories).forEach(function (configProjectName) {
+            var relativeOutputDirectory = config.output_directories[configProjectName];
+            var absoluteOutputDirectory = path.join(configDir, relativeOutputDirectory);
+
+            try {
+                var stats = fs.statSync(absoluteOutputDirectory);
+
+                if (stats.isDirectory() === false) {
+                    console.error('The resolved output directory path specified in the following project in not a directory:');
+                    console.error(' - Project Name:', configProjectName);
+                    console.error(' - Output directory relative to .i3-docs.ini:', relativeOutputDirectory);
+                    console.error(' - Resolved output directory path:', absoluteOutputDirectory);
+                    process.exit(1);
+                }
+            } catch (ex) {
+                console.error('The resolved output directory path specified in the following project does not exist:');
+                console.error(' - Project Name:', configProjectName);
+                console.error(' - Output directory relative to .i3-docs.ini:', relativeOutputDirectory);
+                console.error(' - Resolved output directory path:', absoluteOutputDirectory);
+                process.exit(1);
+            }
+
+            absoluteOutputDirectoryEntries[configProjectName] = absoluteOutputDirectory;
+            log('Found project:', configProjectName, 'at directory:', absoluteOutputDirectory);
+        });
+        log('Identifying projects in .i3-docs.ini [end]');
+
+        return absoluteOutputDirectoryEntries;
+    };
+
+    var installWebAppToAbsoluteDirectoryEntries = function (webAppFilesRoot, absoluteOutputDirectoryEntries) {
+        var webAppFileList = webAppFilesRoot + '*';
+
+        log('Copying web app files to output directories [start]');
+        Object.keys(absoluteOutputDirectoryEntries).forEach(function (configProjectName) {
+            var absoluteOutputDirectory = absoluteOutputDirectoryEntries[configProjectName];
+
+            if (pathIsInside(absoluteOutputDirectory, webAppFilesRoot)) {
+                console.error('Resolved output directory target cannot be inside of web app file root');
+                console.error(' - Resolved output directory path:', absoluteOutputDirectory);
+                console.error(' - Web app files root:', webAppFilesRoot);
+                process.exit(1);
+            }
+
+            log('Starting copy of web app files for project', configProjectName);
+            shelljs.cp('-R', webAppFileList, absoluteOutputDirectory);
+            log('Finish copy of web app files for project', configProjectName);
+        });
+        log('Copying web app files to output directories [end]');
     };
 
     var verifyOnlyOneCommand = function (argv) {
@@ -30,28 +116,18 @@
 
     var install = function (argv) {
         verifyOnlyOneCommand(argv);
+        createLogger(argv.v);
 
         var cwd = process.cwd();
-        var configDir = findParentDir.sync(cwd, '.i3-docs.ini');
+        log('Current Working Directory:', cwd);
 
-        if (configDir === null) {
-            console.error('Could not find .i3-docs.ini config file');
-            process.exit(1);
-        }
+        var webAppFilesRoot = path.join(__dirname, '../build/');
+        log('Web App Files Root', webAppFilesRoot);
 
-        var configPath = path.join(configDir, '.i3-docs.ini');
-        var config = loadI3DocsConfig(configPath);
-
-        var webAppFileList = path.join(__dirname, '../build/*');
-
-        var absoluteOutputDirectories = Object.keys(config.output_directories).map(function (relativeOutputDirectory) {
-            var absoluteOutputDirectory = path.join(configDir, relativeOutputDirectory);
-            return absoluteOutputDirectory;
-        });
-
-        absoluteOutputDirectories.forEach(function (absoluteOutputDirectory) {
-            shelljs.cp('-R', webAppFileList, absoluteOutputDirectory);
-        });
+        var configDir = findI3DocsConfigDir(cwd);
+        var config = loadI3DocsConfig(configDir);
+        var absoluteOutputDirectoryEntries = createAbsoluteOutputDirectoryEntries(configDir, config);
+        installWebAppToAbsoluteDirectoryEntries(webAppFilesRoot, absoluteOutputDirectoryEntries);
     };
 
     var startOfActualArgs = 2;
@@ -60,5 +136,8 @@
          .demandCommand(1)
          .help('h')
          .alias('h', 'help')
+         .boolean('v')
+         .alias('v', 'verbose')
+         .describe('v', 'verbose logging')
          .parse(process.argv.slice(startOfActualArgs));
 }());
